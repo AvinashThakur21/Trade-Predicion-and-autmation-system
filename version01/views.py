607018e329d015a0478310d9b1c552b67d1a2d7b
AvinashThakur21 <thakur352040@gmail.com>
2024-04-26 19:27:07 +0530

    HTML and CSS file configred

--- version01/views.py
+++ version01/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request,'home.html',context={})
